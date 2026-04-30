0e01745ff9d4f5519515f5a8de4e80e25f2aa2d0
Alexey Maksutov <alexey.maksutov@gmail.com>
2026-04-30 17:59:13 -0400

    updates

--- etc/entrypoint/11-timezone.sh
+++ etc/entrypoint/11-timezone.sh
@@ -1,7 +1,7 @@
 [ -z "$TIMEZONE" -a -n "$TZ" ] && TIMEZONE="$TZ"
 [ -z "$TZ" -a -n "$TIMEZONE" ] && TZ="$TIMEZONE"
 [ -z "$TZ" ] && TZ=UTC TIMEZONE=UTC
-[ "$TIMEZONE" != "$TZ"] && echo "Invalid TIMEZONE/TZ" 1>&2 && exit 13
+[ "$TIMEZONE" != "$TZ" ] && echo "Invalid TIMEZONE/TZ" 1>&2 && exit 13
 [ ! -f "/usr/share/zoneinfo/$TZ" ] && echo "Invalid TIMEZONE/TZ" && exit 14
 ln -nsf "/usr/share/zoneinfo/$TZ" /etc/localtime && echo "$TZ" > /etc/timezone
 export TZ TIMEZONE
